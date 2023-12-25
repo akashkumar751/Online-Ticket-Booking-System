5df8131f09eb8429db4bd46abf9c3794504e6018
Akash Kumar <53569572+akashkumar751@users.noreply.github.com>
2023-12-26 00:54:56 +0530

    Add files via upload

---
+++ online.ticket.booking/src/test/java/com/ticket/booking/online/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.booking.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

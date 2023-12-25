16a0356d2547872a3e23f27ce4c0957191bf8f91
Akash Kumar <53569572+akashkumar751@users.noreply.github.com>
2023-12-26 01:04:28 +0530

    Add files via upload

---
+++ online-ticket-booking/src/main/java/com/ticket/booking/onlineticketbooking/OnlineTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.booking.onlineticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketBookingApplication.class, args);
+	}
+
+}
---
+++ online-ticket-booking/src/test/java/com/ticket/booking/onlineticketbooking/OnlineTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.booking.onlineticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
